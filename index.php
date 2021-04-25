006d44637d299f2bad4888e94ea280e6f64f04c5
AprilPan10 <63117386+AprilPan10@users.noreply.github.com>
2021-04-25 14:23:57 -0400

    fix bugs

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ if(isset($_POST['submit'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>Giphy</title>
-    <script type="text/javascript" src="js/jquery-3.5.1.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
@@ -70,6 +69,7 @@ if(isset($_POST['submit'])) {
     </div>
 </form>
 <div id="image"></div>
+<script type="text/javascript" src="js/jquery-3.5.1.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
--- js/function.js
+++ js/function.js
@@ -1,22 +1,15 @@
 function refreshPage(){
     window.location.reload();
-
 }
-var text_to_copy = document.getElementById("para").innerHTML;
-var copy_button = document.getElementById("copy_button");
-
-
 function copy(copyId){
     var $inp=$("<input>");
     $("body").append($inp);
     $inp.val($(""+copyId).text()).select();
     document.execCommand("copy");
     $inp.remove();
-
 }
 $(document).ready(function(){
     $("#copyButton1").click(function(){
         copy('#para');
     });
-
 });
