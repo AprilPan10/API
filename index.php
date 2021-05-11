c77f7df15bfd7dd759dca45def68ed82ede0df8d
AprilPan10 <63117386+AprilPan10@users.noreply.github.com>
2021-05-10 21:50:45 -0400

    add giphy logo

--- index.php
+++ index.php
@@ -71,6 +71,11 @@ if(isset($_POST['submit'])) {
     </div>
 </form>
 <div id="image"></div>
+<footer>
+    <div class="footer">
+        <img src="image/Poweredby_640px-White_VertLogo.png" width="100">
+    </div>
+</footer>
 <script type="text/javascript" src="js/jquery-3.5.1.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
